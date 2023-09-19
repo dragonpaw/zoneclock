5f75dd48817f1121c7c077670ea29d5d070a6722
Ash Arnold <ash@dragonpaw.org>
2023-09-18 23:40:51 -0700

    v2

---
+++ basic.py
@@ -0,0 +1,138 @@
+import time
+import board
+import busio
+import neopixel
+from adafruit_esp32spi.adafruit_esp32spi_wifimanager import (
+    ESPSPI_WiFiManager,
+    adafruit_esp32spi,
+)
+from adafruit_datetime import timezone, timedelta
+from digitalio import DigitalInOut
+
+from _secrets import secrets
+
+WIFI_TEST_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+
+
+class BasicApp:
+    """Provides the common app setup and functionality."""
+
+    def __init__(self):
+        self.brightness = 1.00
+        self.board = board
+        self.cron_jobs = {}
+        self._cron_last_ran = {}
+
+    def cron_run(self):
+        """Check for and run due cron jobs.
+
+        Cron jobs are defined as a dict of method names, to intervals(in seconds)
+        they should run."""
+
+        now = time.time()
+        for job in self.cron_jobs:
+            if job not in self._cron_last_ran or (
+                self._cron_last_ran[job] + self.cron_jobs[job] < now
+            ):
+                print(f"Running scheduled job: {job}")
+                getattr(self, job)()
+                self._cron_last_ran[job] = now
+
+    def network_setup(self) -> ESPSPI_WiFiManager:
+        """Activate the network."""
+        esp32_cs = DigitalInOut(board.ESP_CS)
+        esp32_ready = DigitalInOut(board.ESP_BUSY)
+        esp32_reset = DigitalInOut(board.ESP_RESET)
+        spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+        esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+        self.network = ESPSPI_WiFiManager(
+            esp,
+            secrets,
+            status_pixel=neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2),
+            debug=True,
+        )
+
+    def network_test(self):
+        """Test that the network is working by fetching and printing data"""
+        # esp._debug = True
+        print("Fetching text from", WIFI_TEST_URL)
+        r = self.network.get(WIFI_TEST_URL)
+        print("-" * 40)
+        print(r.text)
+        print("-" * 40)
+        r.close()
+
+        print("Done!")
+
+    def parse_time(self, timestring, is_dst=-1):
+        """Given a string of the format YYYY-MM-DDTHH:MM:SS.SS-HH:MM (and
+        optionally a DST flag), convert to and return an equivalent
+        time.struct_time (strptime() isn't available here). Calling function
+        can use time.mktime() on result if epoch seconds is needed instead.
+        Time string is assumed local time; UTC offset is ignored. If seconds
+        value includes a decimal fraction it's ignored.
+        """
+        import time
+
+        date_time = timestring.split("T")  # Separate into date and time
+        year_month_day = date_time[0].split("-")  # Separate time into Y/M/D
+        hour_minute_second = date_time[1].split("+")[0].split("-")[0].split(":")
+        return time.struct_time(
+            (
+                int(year_month_day[0]),
+                int(year_month_day[1]),
+                int(year_month_day[2]),
+                int(hour_minute_second[0]),
+                int(hour_minute_second[1]),
+                int(hour_minute_second[2].split(".")[0]),
+                -1,
+                -1,
+                is_dst,
+            )
+        )
+
+    def lookup_timezone(self, name: str, set_rtc=False):
+        """Update system date/time from WorldTimeAPI public server;
+        no account required. Pass in time zone string
+        (http://worldtimeapi.org/api/timezone for list)
+        or None to use IP geolocation. Returns current local time as a
+        time.struct_time and UTC offset as string. This may throw an
+        exception on fetch_data() - it is NOT CAUGHT HERE, should be
+        handled in the calling code because different behaviors may be
+        needed in different situations (e.g. reschedule for later).
+        """
+        import rtc
+
+        time_url = f"http://worldtimeapi.org/api/timezone/{name}"
+
+        print(f"Fetching time from {time_url}...")
+        time_data: dict = self.network.get(
+            time_url, headers={"Accept": "application/json"}
+        ).json()
+
+        # This is here because we sync the clock via the same API as lookup TZ offsets.
+        if set_rtc:
+            time_struct = self.parse_time(
+                timestring=time_data["datetime"], is_dst=time_data["dst"]
+            )
+            rtc.RTC().datetime = time_struct
+            print("RTC updated from Internet: %s" % time.localtime())
+
+        delta = timedelta(seconds=time_data["raw_offset"])
+        if time_data["dst"]:
+            delta += timedelta(seconds=time_data["dst_offset"])
+        print("{:20s} {:d}".format(time_data["timezone"], delta.seconds))
+        return timezone(offset=delta, name=time_data["timezone"])
+
+    def color(self, color_in_hex: int) -> int:
+        """Apply brightness to a color.
+
+        This is the math you do when the board don't support brightness setting.
+        """
+
+        r = int(round((color_in_hex >> 16) * self.brightness, 0))
+        g = int(round((color_in_hex >> 8 & 0xFF) * self.brightness, 0))
+        b = int(round((color_in_hex & 0xFF) * self.brightness, 0))
+        # print(f"r:{r}, g:{g}, b:{b}")
+        return r << 16 | g << 8 | b
--- main.py
+++ main.py
@@ -8,141 +8,174 @@ Ash's multi-TZ wall clock
 
 import gc
 import time
-import math
 import displayio
 import terminalio
 from adafruit_datetime import datetime
 from adafruit_display_text.label import Label
 from adafruit_matrixportal.matrix import Matrix
+from adafruit_datetime import timezone
 
 import colors2 as colors
-from networking import network_setup, network_test, lookup_timezone
+import basic
 
-MATRIX = Matrix(bit_depth=6, width=32, height=32)
-# DISPLAY: framebufferio.FramebufferDisplay = MATRIX.display
-# DISPLAY.brightness = 0.2
-DISPLAY = MATRIX.display
-DISPLAY.brightness = 0.2
-NETWORK: ESPSPI_WiFiManager = None
-
-TIMEZONE_NAMES = [
+TIMEZONES_to_SHOW = [
     "America/Los_Angeles",
     "Europe/Berlin",
     "Europe/Athens",
     "Asia/Kuala_Lumpur",
 ]
-COUNTRY_CODES = ["US", "DE", "GR", "MY"]
-TIMEZONES = []
-COLORS = [
-    [colors.RED, colors.WHITE, colors.BLUE],
-    [colors.GRAY, colors.RED, colors.GOLD],
-    [colors.BLUE, colors.WHITE, colors.BLUE],
-    [colors.YELLOW, colors.BLUE, colors.RED],
-]
-FONT = terminalio.FONT
-# SMALL_FONT = bitmap_font.load_font("/fonts/helvR10.bdf")
-# SMALL_FONT.load_glyphs("0123456789:/.%")
-# print("Fonts loaded")
-LINE_SPACING = 0.75
-GROUP: displayio.Group = displayio.Group(x=2, y=0)
-GROUP.append(
-    Label(
-        FONT, text="Booting", color=colors.ORCHID, x=0, y=10, line_spacing=LINE_SPACING
-    )
-)
-DISPLAY.root_group = GROUP
+
+COUNTRY_CODES = {
+    "America/Los_Angeles": "US",
+    "Europe/Berlin": "DE",
+    "Europe/Athens": "GR",
+    "Asia/Kuala_Lumpur": "MY",
+}
+
+FLAG_COLORS = {
+    "US": [colors.RED, colors.WHITE, colors.BLUE],
+    "DE": [colors.GRAY, colors.RED, colors.GOLD],
+    "GR": [colors.BLUE, colors.WHITE, colors.BLUE],
+    "MY": [colors.YELLOW, colors.BLUE, colors.RED],
+}
 
 
-def init() -> None:
-    global NETWORK, GROUP, TIMEZONES
+class ZoneClock(basic.BasicApp):
+    """A clock for showing 4 time zones on a 32x32 display."""
 
-    GROUP[0].color = colors.PURPLE
-    GROUP[0].text = "WiFi\nSetup"
-    # DISPLAY.show()
-    NETWORK = network_setup()
-    network_test(requests=NETWORK)
-    GROUP[0].text = "Time\nSync"
-    # DISPLAY.show()
-    lookup_timezone(requests=NETWORK, name="UTC", set_rtc=True)
-    GROUP[0].text = "Time\nZones"
-    TIMEZONES = [
-        lookup_timezone(requests=NETWORK, name=tz, set_rtc=False)
-        for tz in TIMEZONE_NAMES
-    ]
+    def __init__(self, timezone_names: list[str]) -> None:
+        super().__init__()
 
+        # Board level variables
+        self.matrix = Matrix(bit_depth=6, width=32, height=32)
+        self.display = self.matrix.display
+        self.brightness = 0.2
+        self.display.brightness = self.brightness  # Doesn't seem to do anything sadly.
 
-def display_group() -> displayio.Group:
-    group = displayio.Group(x=0, y=4)
-    font_width, font_height = FONT.get_bounding_box()
-    font_width -= 1
-    line_height = 8
-    now = datetime.now()
-    for i in range(len(TIMEZONES)):
-        y = i * line_height
-        group.append(
-            Label(
-                FONT,
-                text="{:2d}".format((now + TIMEZONES[i].utcoffset(None)).hour),
-                color=COLORS[i][0],
-                x=0,
-                y=y,
-            )
+        # The sadly ugly font we use
+        self.font = terminalio.FONT
+        self.line_spacing = 0.75
+
+        # The zones we will be monitoring.
+        self.timezone_names = timezone_names
+
+        # The display group to use for the life of the app
+        self.display_group: displayio.Group = displayio.Group(x=2, y=0)
+        self.display.root_group = self.display_group
+
+        # The status label we will update while starting up.
+        self.status_label = Label(
+            self.font,
+            text="Booting",
+            color=self.color(colors.ORCHID),
+            x=0,
+            y=10,
+            line_spacing=self.line_spacing,
         )
-        if i == 0:
+        self.display_group.append(self.status_label)
+
+        # WiFi setup
+        self.status_label.color = self.color(colors.PURPLE)
+        self.status_label.text = "WiFi\nSetup"
+        self.network_setup()
+
+        # Network test
+        self.status_label.text = "WiFi\nTest"
+        self.network_test()
+
+        self.cron_jobs = {
+            "set_rtc": 60 * 60,  # Yes, the RTC sucks that bad.
+            "set_timezones": 60 * 60 * 24,  # Daily, check for offset change
+        }
+
+    def set_timezones(self) -> list[timezone]:
+        """Lookup all the current TZ offsets.
+
+        Needs to be called to update when time zones change. (daily)"""
+
+        self.status_label.text = "Time\nZones"
+        self.timezones = [
+            self.lookup_timezone(name=n, set_rtc=False) for n in self.timezone_names
+        ]
+
+    def set_rtc(self):
+        self.status_label.text = "Time\nSync"
+        self.lookup_timezone(name="UTC", set_rtc=True)
+
+    def time_group(self) -> displayio.Group:
+        group = displayio.Group(x=0, y=4)
+        font_width, font_height = self.font.get_bounding_box()
+        font_width -= 1
+        line_height = 8
+        now = datetime.now()
+        for i, tz in enumerate(self.timezones):
+            y = i * line_height
+            tz_name = tz._name  # There's no property for this.
+            country_code = COUNTRY_CODES[tz_name]
+            flag_colors = FLAG_COLORS[country_code]
+            print("Flag colors: {0} = {1}".format(tz_name, repr(flag_colors)))
             group.append(
                 Label(
-                    FONT,
-                    text=":",
-                    color=COLORS[i][1],
-                    x=font_width * 2,
-                    y=y - 1,
-                )
-            )
-            group.append(
-                Label(
-                    FONT,
-                    text="{:02d}".format(now.minute),
-                    color=COLORS[i][2],
-                    x=font_width * 2 + 4,
+                    self.font,
+                    text="{:2d}".format((now + tz.utcoffset(None)).hour),
+                    color=self.color(flag_colors[0]),
+                    x=0,
                     y=y,
                 )
             )
-        else:
-            group.append(
-                Label(
-                    FONT,
-                    text=COUNTRY_CODES[i][0],
-                    color=COLORS[i][1],
-                    x=font_width * 2 + 4,
-                    y=y,
+            if i == 0:
+                # First row has the actual minutes of the hour
+                group.append(
+                    Label(
+                        self.font,
+                        text=":",
+                        color=self.color(flag_colors[1]),
+                        x=font_width * 2,
+                        y=y - 1,
+                    )
                 )
-            )
-            group.append(
-                Label(
-                    FONT,
-                    text=COUNTRY_CODES[i][1],
-                    color=COLORS[i][2],
-                    x=font_width * 3 + 5,
-                    y=y,
+                group.append(
+                    Label(
+                        self.font,
+                        text="{:02d}".format(now.minute),
+                        color=self.color(flag_colors[2]),
+                        x=font_width * 2 + 4,
+                        y=y,
+                    )
                 )
-            )
-    return group
+            else:
+                # Every other row, instead of the minutes, the country code is shown.
+                group.append(
+                    Label(
+                        self.font,
+                        text=country_code[0],
+                        color=self.color(flag_colors[1]),
+                        x=font_width * 2 + 4,
+                        y=y,
+                    )
+                )
+                group.append(
+                    Label(
+                        self.font,
+                        text=country_code[1],
+                        color=self.color(flag_colors[2]),
+                        x=font_width * 3 + 5,
+                        y=y,
+                    )
+                )
+        return group
+
+    def main(self) -> None:
+        while True:
+            print("Time: ", datetime.now())
+            gc.collect()
+            self.cron_run()
+            self.display.root_group = self.time_group()
+
+            time.sleep(10)
 
 
-def main() -> None:
-    init()
-
-    # GROUP: displayio.Group = display_group()
-
-    while True:
-        print("Time: ", datetime.now())
-        gc.collect()
-        DISPLAY.root_group = display_group()
-        # DISPLAY.refresh()
-        # DISPLAY.auto_brightness = True
-        # DISPLAY.show()
-        time.sleep(10)
-
+app = ZoneClock(TIMEZONES_to_SHOW)
 
 if __name__ == "__main__":
-    main()
+    app.main()
